38605d974e12d7afeecdce38237bebaaa36fe023
jikida <kubalajiri@volny.cz>
2025-10-28 18:57:12 +0100

    Add files via upload

---
+++ backup2.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+DirToBackup=/home/sep/sep-network-disk
+DOW=$(date +"%a")
+#BackupDir=/mnt/$DOW
+BackupDir2=/mnt2/$DOW
+#LogFile=/mnt/backup_logs/backup-$(date "+%Y-%m-%d %H:%M:%S").log
+LogFile=/mnt2/backup_logs/backup-$(date "+%Y-%m-%d %H:%M:%S").log
+
+
+#rsync --recursive --update --delete --perms --owner --group --times \
+# --links --safe-links --one-file-system --devices --log-file="$LogFile" $DirToBackup $BackupDir
+
+rsync --recursive --update --delete --perms --owner --group --times \
+ --links --safe-links --one-file-system --devices --log-file="$LogFile" $DirToBackup $BackupDir2
